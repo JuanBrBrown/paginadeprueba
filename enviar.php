a07ff29f71867b931c4ff0babb5813b272d07d46
JuanBrBrown <70238958+JuanBrBrown@users.noreply.github.com>
2020-08-26 08:55:33 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -10,6 +10,6 @@ $header = 'Enviado desde la página de JuanBrBrown Web designer';
 $mensajeCompleto = "Nombre" . $nombre . "\nCorreo" . $correo . "\nTelefono " . $telefono . "\nMensaje " . $mensaje;
 
 mail($destinatario, $mensajeCompleto, $header);
-echo "<script> setTimeout(\"location.href='index.html'\", 1000)</script>"
+header("index.html");
 
 ?>
